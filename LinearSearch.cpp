f281ba5fc582e0dfe8ee0c4bbe4e008b18a42c93
ferdous05 <135028375+ferdous05@users.noreply.github.com>
2024-05-21 09:03:24 +0600

    Add files via upload

---
+++ LinearSearch.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+/*void Linearsearch (int Arr[], int size, int matching_number)
+{
+
+int Store ;
+
+    for (int i=0; i<size; i++)
+    {
+
+        if (Arr[i]==matching_number)
+        {
+         Store=   i;
+        }
+        cout<<"The Position "<<Store;
+
+    }
+
+
+
+}*/
+
+
+int linear_Ser(int Arr[], int size,int num){
+
+
+int store=0;
+for (int i=0;i<size;i++){
+
+    if (Arr[i]==num){
+        return i;
+    }
+
+
+
+}
+
+return -1;
+
+
+
+}
+int main (void)
+{
+
+    int x;
+    cout<<"input the size\n";
+    cin>>x;
+    int Array[x];
+
+    int fn;
+    cout<<"input the Finding Number\n";
+    cin>>fn;
+
+
+    cout<<"input the Values\n";
+
+
+    for (int i=0; i<x; i++)
+    {
+
+        cin>>Array[i];
+
+    }
+    if (linear_Ser(Array, x,fn)==-1){
+        cout<<"The value is not present in Arrary";
+    }
+
+
+   cout<<"The Output: " <<linear_Ser(Array, x,fn);
+
+
+
+
+
+}
+
+
