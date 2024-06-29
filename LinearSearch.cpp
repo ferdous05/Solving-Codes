374c7b0cc8cc5a67570fe842ec7b71cbf996a542
ferdous05 <135028375+ferdous05@users.noreply.github.com>
2024-06-29 11:13:04 +0600

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -2,23 +2,14 @@
 using namespace std;
 
 int linear_Ser(int Arr[], int size,int num){
-
-
 int store=0;
 for (int i=0;i<size;i++){
 
     if (Arr[i]==num){
         return i;
     }
-
-
-
 }
-
 return -1;
-
-
-
 }
 int main (void)
 {
@@ -31,25 +22,15 @@ int main (void)
     int fn;
     cout<<"input the Finding Number\n";
     cin>>fn;
-
-
     cout<<"input the Values\n";
-
-
     for (int i=0; i<x; i++)
     {
-
         cin>>Array[i];
-
     }
     if (linear_Ser(Array, x,fn)==-1){
         cout<<"The value is not present in Arrary";
     }
-
-
    cout<<"The Output: " <<linear_Ser(Array, x,fn);
-
-
 
 
 
